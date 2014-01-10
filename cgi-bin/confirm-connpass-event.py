926e96149ad40d345152cc0988edf43a6153db30
Atsuo Ishimoto <ishimoto@gembook.org>
2014-01-11 00:57:20 +0900

    Make json an attachment.

--- cgi-bin/confirm-connpass-event.py
+++ cgi-bin/confirm-connpass-event.py
@@ -4,8 +4,11 @@ import cgitb
 cgitb.enable()
 
 import os, sys, cgi, re, json, urllib2, string, hashlib, os, time, subprocess
+from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
+from email.mime.application import MIMEApplication
 from email.Header import Header
+from email.encoders import encode_base64
 
 
 JSON_DIR = '/var/www/connpass-reqs/'
@@ -36,12 +39,16 @@ def sendmail(reqid):
     mailaddr = 'ishimoto@gembook.org'
     with open(os.path.join(JSON_DIR, reqid), 'r') as f:
         s = f.read()
-
-    mail = MIMEText(msg % (reqid, s), _charset='iso-2022-jp')
+    mail = MIMEMultipart()
     mail['From'] = 'noreply@python.jp'
     mail['To'] = mailaddr
     mail['Subject'] = Header(u'Python.jp イベント登録の確認', 'iso-2022-jp')
 
+    msg.attach(MIMEText(msg % (reqid, s), _charset='iso-2022-jp'))
+    j = MIMEApplication(s, 'octet-stream', encode_base64)
+    j.add_header('Content-Disposition', 'attachment; filename="%s"' % reqid.json)
+    msg.attach(j)
+    
     p = subprocess.Popen([SENDMAIL, mailaddr], stdin=subprocess.PIPE,
                          stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     o, e =  p.communicate(mail.as_string())
